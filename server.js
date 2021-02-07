0e9fcc1939c2b54dce750546c3846918ef3224da
Nikita Babko <niki-babko@yandex.ru>
2021-02-07 22:37:57 +0700

    Added comments, Map object and connect to socket.

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
-const express = require('express');
+const app = require('express')();
+/**
+ * Указываем, что наш сервер будет работать через переменную 'app'.
+ * И теперь в 'server' хранится наше приложение.
+ */
+const server = require('http').Server(app);
+/**
+ * Говорим, что переменная 'io' в переменную 'server'
+ * помещает возможность работы с веб-сокетами.
+ * Теперь переменная 'server' знает, что такое сокеты.
+ * Сокет теперь понимает, что есть какой-то сервер.
+ * Переменная 'io' - теперь будет хранить информацию о сокетах, и информацию о сервере.
+ */
+const io = require('socket.io')(server, { cors: { origin: '*' } });
 
-const app = express();
-
-const rooms = {
-  rooms: [],
-  messages: ['hello', 'hi'],
-};
+const rooms = new Map();
 
 app.get('/rooms', (req, res) => {
-  console.log('Hello');
+  rooms.set('hello', '');
   res.json(rooms);
+});
+
+io.on('connection', (socket) => {
+  console.log('Connected', socket.id);
 });
 
 // Server start
 const PORT = process.env.PORT || 9999;
-app.listen(PORT, (err) => {
+server.listen(PORT, (err) => {
   if (err) {
     throw Error(err);
   }
