3849a96db679f51010460e7b48e8506f04ce7c14
Nikita Babko <niki-babko@yandex.ru>
2021-02-10 15:05:32 +0700

    Added reducer, state, action.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
+import reducer from 'reducer';
 import JoinBlock from 'components/JoinBlock.jsx';
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    isAuth: false,
+  });
+
+  const onSignIn = () => {
+    dispatch({
+      type: 'IS_AUTH',
+      payload: true,
+    });
+  };
+
   return (
     <div className="wrapper">
-      <JoinBlock />
+      {!state.isAuth ? <JoinBlock onSignIn={onSignIn} /> : <h1>Welcome</h1>}
     </div>
   );
 };
--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import axios from 'axios';
 
 import socket from 'socket';
 
-const JoinBlock = () => {
+const JoinBlock = ({ onSignIn }) => {
+  const [roomId, setRoomId] = useState('');
+  const [userName, setUserName] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onEnter = async () => {
+    if (!roomId || !userName) {
+      return alert('Wrong data!');
+    }
+    setIsLoading(true);
+    await axios
+      .post('/rooms', {
+        roomId,
+        userName,
+      })
+      .then(onSignIn);
+  };
+
   return (
     <div className="join-block">
-      <input type="text" placeholder="Room ID" />
-      <input type="text" placeholder="Your name" />
-      <button className="btn btn-success">Enter</button>
+      <input
+        type="text"
+        placeholder="Room ID"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Your name"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button disabled={isLoading} onClick={onEnter} className="btn btn-success">
+        {isLoading ? 'Loading...' : 'Enter'}
+      </button>
     </div>
   );
 };
