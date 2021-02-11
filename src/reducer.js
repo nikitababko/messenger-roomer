8f022bdb1a8d149ee2ea0e958451f3bb00185d4b
Nikita Babko <niki-babko@yandex.ru>
2021-02-11 09:29:21 +0700

    Added SET_DATA, SET_USERS, NEW_MESSAGE.

--- src/reducer.js
+++ src/reducer.js
@@ -8,6 +8,25 @@ export default (state, action) => {
         userName: action.payload.userName,
       };
 
+    case 'SET_DATA':
+      return {
+        ...state,
+        users: action.payload.users,
+        messages: action.payload.messages,
+      };
+
+    case 'SET_USERS':
+      return {
+        ...state,
+        users: action.payload,
+      };
+
+    case 'NEW_MESSAGE':
+      return {
+        ...state,
+        messages: [...state.messages, action.payload],
+      };
+
     default:
       return state;
   }
