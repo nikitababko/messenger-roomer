ce77d29c8ddd0de69d506e9d9ea77e8ba9482986
Nikita Babko <niki-babko@yandex.ru>
2021-02-11 09:30:28 +0700

    Added onAddMessage and async-get.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
+
+import axios from 'axios';
 
 import socket from 'socket';
-
 import reducer from 'reducer';
 import JoinBlock from 'components/JoinBlock.jsx';
+import Chat from 'components/Chat.jsx';
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, {
     joined: false,
     roomId: null,
     userName: null,
+    users: [],
+    messages: [],
   });
 
-  const onSignIn = (obj) => {
+  const onSignIn = async (obj) => {
     dispatch({
       type: 'JOINED',
       payload: obj,
     });
     socket.emit('ROOM:JOIN', obj);
+    const { data } = await axios.get(`/rooms/${obj.roomId}`);
+    dispatch({
+      type: 'SET_DATA',
+      payload: data,
+    });
   };
 
-  console.log(state);
+  const setUsers = (users) => {
+    dispatch({
+      type: 'SET_USERS',
+      payload: users,
+    });
+  };
+
+  const onAddMessage = (message) => {
+    dispatch({
+      type: 'NEW_MESSAGE',
+      payload: message,
+    });
+  };
+
+  useEffect(() => {
+    socket.on('ROOM:SET_USERS', setUsers);
+    socket.on('ROOM:NEW_MESSAGE', onAddMessage);
+  }, []);
 
   return (
     <div className="wrapper">
-      {!state.joined ? <JoinBlock onSignIn={onSignIn} /> : <h1>Welcome</h1>}
+      {!state.joined ? (
+        <JoinBlock onSignIn={onSignIn} />
+      ) : (
+        <Chat {...state} onAddMessage={onAddMessage} />
+      )}
     </div>
   );
 };
