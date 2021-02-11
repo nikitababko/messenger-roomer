3fc955d1377f374ef4ebb39cea98b23842508e9b
Nikita Babko <niki-babko@yandex.ru>
2021-02-11 09:33:07 +0700

    Added ROOM:NEW_MESSAGE.

--- server.js
+++ server.js
@@ -20,8 +20,15 @@ app.use(express.json());
 
 const rooms = new Map();
 
-app.get('/rooms', (req, res) => {
-  res.json(rooms);
+app.get('/rooms/:id', (req, res) => {
+  const { id: roomId } = req.params;
+  const obj = rooms.has(roomId)
+    ? {
+        users: [...rooms.get(roomId).get('users').values()],
+        messages: [...rooms.get(roomId).get('messages').values()],
+      }
+    : { users: [], messages: [] };
+  res.json(obj);
 });
 
 app.post('/rooms', (req, res) => {
@@ -39,8 +46,37 @@ app.post('/rooms', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('ROOM:JOIN', (data) => {
-    console.log(data);
+  socket.on('ROOM:JOIN', ({ roomId, userName }) => {
+    // Подлючение к сокету в определенную комнату
+    socket.join(roomId);
+    // Сохраняем это все в нашей "базе данных"
+    rooms.get(roomId).get('users').set(socket.id, userName);
+    // Получаем список всех пользователей
+    const users = [...rooms.get(roomId).get('users').values()];
+    /**
+     * Говорим, что в определенную комнату,
+     * всем кроме меня - нужно отправить сокет-запрос - 'ROOM:JOINED'
+     * ROOM:JOINED - передает список всех пользователей
+     */
+    socket.to(roomId).broadcast.emit('ROOM:SET_USERS', users);
+  });
+
+  socket.on('ROOM:NEW_MESSAGE', ({ roomId, userName, text }) => {
+    const obj = {
+      userName,
+      text,
+    };
+    rooms.get(roomId).get('messages').push(obj);
+    socket.to(roomId).broadcast.emit('ROOM:NEW_MESSAGE', obj);
+  });
+
+  socket.on('disconnect', () => {
+    rooms.forEach((value, roomId) => {
+      if (value.get('users').delete(socket.id)) {
+        const users = [...value.get('users').values()];
+        socket.to(roomId).emit('ROOM:SET_USERS', users);
+      }
+    });
   });
 
   console.log('Connected', socket.id);
